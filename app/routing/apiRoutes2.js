067dd80a91374ba5ac4c213eeaa98260be63cbb6
kbyard <38507856+kbyard@users.noreply.github.com>
2018-09-08 13:58:03 -0700

    Add files via upload

---
+++ app/routing/apiRoutes2.js
@@ -0,0 +1,33 @@
+var friends = require("../data/friends");
+
+module.exports = function (app) {
+    app.get("/api/friendfinder", function (req, res) {
+        res.json(friends);
+    });
+
+    app.post("/api/friendfinder", function (req, res) {
+        friends.push(req.body);
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 50
+        };
+
+        var userData = req.body;
+        var totalDifference = 0;
+
+        for (var i = 0; i < (friends.length - 1) ; i++) {
+            totalDifference = 0;
+            for (var j = 0; j < friends[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userData.scores[j]) - parseInt(friends[i].scores[j]));
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+
+        res.json(bestMatch);
+    });
+};
